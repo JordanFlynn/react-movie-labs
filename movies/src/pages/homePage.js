acce413d168f09d476fec3cab85bb8d42ee8d4b2
Jordan Flynn <20096714@mail.wit.com>
2023-11-26 01:28:37 +0000

    Home page rendering

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -22,19 +22,3 @@ const MovieListPage = (props) => {
   );
 };
 export default MovieListPage;
-Also, create the file src/components/movieList/index.js (referenced by the above code) and add this code:
-
-import React from "react";
-import Movie from "../movieCard/";
-import Grid from "@mui/material/Grid";
-
-const MovieList = (props) => {
-  let movieCards = props.movies.map((m) => (
-    <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Movie key={m.id} movie={m} />
-    </Grid>
-  ));
-  return movieCards;
-};
-
-export default MovieList;
