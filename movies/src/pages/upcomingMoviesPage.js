6f03906eb7fa76534b333f4806ae3b6e8a857f21
Jordan Flynn <20096714@mail.wit.com>
2023-11-26 20:21:36 +0000

    All Working

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -4,11 +4,7 @@ import { getMovies, getUpcomingMovies } from "../api/tmdb-api";
 
 
 const UpcomingMoviesPage = (props) => {
-      const toDo = () => true;
-  // Get movies from local storage.
-//   const upcomingMovies = JSON.parse(localStorage.getItem("favorites")); 
     const [movies, setMovies] = useState([]);
-    // const [upcoming] = useState([]);
 
     useEffect(() => {
         getUpcomingMovies().then(movies => {
